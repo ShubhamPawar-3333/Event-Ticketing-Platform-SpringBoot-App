b2be9a17efd0d619631fce15afd1685fb191c56e
ShubhamPawar-3333 <shubham.d.pawar.3333@gmail.com>
2025-06-28 08:58:33 +0530

    Add TicketValidation class and validations reference to Ticket class

--- src/main/java/com/shubhamP/Portfolio/eventTicketingApplication/domain/TicketValidation.java
+++ src/main/java/com/shubhamP/Portfolio/eventTicketingApplication/domain/TicketValidation.java
@@ -6,18 +6,16 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Entity
-@Table(name = "tickets")
+@Table(name = "ticket_validations")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Ticket {
+public class TicketValidation {
 
     @Id
     @Column(name = "id", nullable = false, updatable = false)
@@ -28,19 +26,13 @@ public class Ticket {
     @Enumerated(EnumType.STRING)
     private TicketStatusEnum status;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "ticket_type_id")
-    private TicketType ticketType;
+    @Column(name = "validation_methods", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketValidationMethod validationMethod;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "purchaser_id")
-    private User purchaser;
-
-    @OneToMany(mappedBy = "ticket", cascade = CascadeType.ALL)
-    private List<TicketValidation> validations = new ArrayList<>();
-
-    // TODO:QRCode
-
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
 
     @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
@@ -49,6 +41,4 @@ public class Ticket {
     @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updated;
-
-
 }
