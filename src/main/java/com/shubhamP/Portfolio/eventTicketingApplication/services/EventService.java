d22ecfba92e525c41dc1185581e8d5cc57df8fec
ShubhamPawar-3333 <shubham.d.pawar.3333@gmail.com>
2026-01-05 18:42:54 +0530

    Add Update Event Design

--- src/main/java/com/shubhamP/Portfolio/eventTicketingApplication/services/EventService.java
+++ src/main/java/com/shubhamP/Portfolio/eventTicketingApplication/services/EventService.java
@@ -1,7 +1,9 @@
 package com.shubhamP.Portfolio.eventTicketingApplication.services;
 
 import com.shubhamP.Portfolio.eventTicketingApplication.domain.CreateEventRequest;
+import com.shubhamP.Portfolio.eventTicketingApplication.domain.UpdateEventRequest;
 import com.shubhamP.Portfolio.eventTicketingApplication.domain.entities.Event;
+import com.sun.jdi.request.EventRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -12,4 +14,5 @@ public interface EventService {
     Event createEvent(UUID organizerId,  CreateEventRequest event);
     Page<Event> listEventsForOrganizer(UUID organizerId, Pageable pageable);
     Optional<Event> getEventForOrganizer(UUID organizerId, UUID id);
+    Event updateEventForOrganizer(UUID organizerId, UUID id, UpdateEventRequest event);
 }
